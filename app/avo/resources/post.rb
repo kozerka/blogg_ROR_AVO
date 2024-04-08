cc3d1dd78719d5dd211ac9bce0687ad45d349691
kozerka <kozerka@gmail.com>
2024-04-08 11:52:34 +0200

    Chore: Initial app setup for avo integration

---
+++ app/avo/resources/post.rb
@@ -0,0 +1,12 @@
+class Avo::Resources::Post < Avo::BaseResource
+  self.includes = []
+  # self.search = {
+  #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
+  # }
+
+  def fields
+    field :id, as: :id
+    field :title, as: :text
+    field :body, as: :textarea
+  end
+end
